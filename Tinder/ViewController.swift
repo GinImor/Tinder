8eb2981041533b77f846125e04200c03e9af3015
Gin Imor <openheart23@163.com>
2021-03-21 19:47:53 +0800

    Initial set up

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Gin Imor on 3/21/21.
+//  Copyright © 2021 Brevity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemBackground
+  }
+
+
+}
+
